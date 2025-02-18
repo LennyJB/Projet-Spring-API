276b2690c7980982ac54542c107b831fb347caec
Lenny Jean-Baptiste <lennyjb@outlook.fr>
2025-02-18 09:45:29 +0100

    init

---
+++ ProjetSpringAPI/src/main/java/efrei/projetspringapi/ProjetSpringApiApplication.java
@@ -0,0 +1,13 @@
+package efrei.projetspringapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetSpringApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjetSpringApiApplication.class, args);
+    }
+
+}
---
+++ ProjetSpringAPI/src/test/java/efrei/projetspringapi/ProjetSpringApiApplicationTests.java
@@ -0,0 +1,13 @@
+package efrei.projetspringapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetSpringApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
